5d5afba93e3cc5febbd93729177de3566f03db28
zjhozella <z.hozella@gmail.com>
2022-04-14 00:34:55 -0400

    Income Database Insertion

--- app/src/main/java/com/example/budgetingessentials/DashboardFragment.java
+++ app/src/main/java/com/example/budgetingessentials/DashboardFragment.java
@@ -1,17 +1,33 @@
 package com.example.budgetingessentials;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +50,10 @@ public class DashboardFragment extends Fragment {
     // Floating action button animation
     private Animation fadeOpen, fadeClose;
 
+    // Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -81,6 +101,14 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+
         // Connect floating buttons and texts to layout
         fab_main = myView.findViewById(R.id.fb_main_btn);
         fab_income = myView.findViewById(R.id.income_ft_btn);
@@ -95,6 +123,9 @@ public class DashboardFragment extends Fragment {
         fab_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                addData();
+
                 if (isOpen){
                     fab_income.startAnimation(fadeClose);
                     fab_expense.startAnimation(fadeClose);
@@ -125,4 +156,98 @@ public class DashboardFragment extends Fragment {
 
         return myView;
     }
+
+    private void addData(){
+
+        // Fab Button Income
+        fab_income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incomeDataInsert();
+            }
+        });
+
+        // Fab Button Expese
+        fab_expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    public void incomeDataInsert(){
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+        View myView = inflater.inflate(R.layout.layout_insert_data, null);
+        myDialog.setView(myView);
+        AlertDialog dialog = myDialog.create();
+
+        EditText edtAmount = myView.findViewById(R.id.amount_edt);
+        EditText edtType = myView.findViewById(R.id.type_edt);
+        EditText edtNote = myView.findViewById(R.id.note_edt);
+
+        Button btnSave = myView.findViewById(R.id.btnSave);
+        Button btnCancel = myView.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = edtType.getText().toString().trim();
+                String amount = edtAmount.getText().toString().trim();
+                String note = edtNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    edtType.setError("Required Field...");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(amount)){
+                    edtAmount.setError("Required Field...");
+                    return;
+                }
+
+                int int_amount = Integer.parseInt(amount);
+
+                if(TextUtils.isEmpty(note)){
+                    edtNote.setError("Required Field...");
+                    return;
+                }
+
+                String id = mIncomeDatabase.push().getKey();
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(int_amount, type, note, id, mDate);
+                mIncomeDatabase.child(id).setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getActivity(), "Data Added Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Data Addition Failed", Toast.LENGTH_SHORT).show();
+                                System.out.println("DATA UPLOAD FIREBASE FAILED: " + e);
+                            }
+                        });;
+
+
+                dialog.dismiss();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
 }
