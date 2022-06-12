68f2df0f44f882ac6a887d7c217860a708d71e5f
zjhozella <z.hozella@gmail.com>
2022-06-12 12:13:33 -0400

    Navigation Drawer
    Implemented the navigation drawer, floating action button is no longer working. Will attempt to move the floating action button to the main layout to be shared across all fragments.

--- app/src/main/java/com/hozella/budgetingessentials/HomeActivity.java
+++ app/src/main/java/com/hozella/budgetingessentials/HomeActivity.java
@@ -1,23 +1,25 @@
 package com.hozella.budgetingessentials;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.FrameLayout;
+
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
@@ -35,6 +37,21 @@ public class HomeActivity extends AppCompatActivity /*implements NavigationView.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.app_toolbar);
+        toolbar.setTitle("Expense Manager");
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         bottomNavigationView = findViewById(R.id.bottomNavBar);
         frameLayout = findViewById(R.id.main_frame);
@@ -80,4 +97,55 @@ public class HomeActivity extends AppCompatActivity /*implements NavigationView.
 
     }
 
+    @Override
+    public void onBackPressed(){
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    public void displaySelectedListener(int itemId){
+        Fragment fragment = null;
+
+        switch (itemId){
+            case R.id.nav_dashboard:
+                setFragment(dashboardFragment);
+                DashboardFragment.isOpen = false;
+                break;
+
+            case R.id.nav_income:
+                setFragment(incomeFragment);
+                DashboardFragment.isOpen = false;
+                break;
+
+            case R.id.nav_expense:
+                setFragment(expenseFragment);
+                DashboardFragment.isOpen = false;
+                break;
+
+            case R.id.nav_logout:
+
+                break;
+        }
+
+        if (fragment != null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        displaySelectedListener(item.getItemId());
+
+        return true;
+    }
 }
